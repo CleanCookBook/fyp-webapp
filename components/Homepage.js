5adadee888b72e8fd37b1af9970760abdfbac570
Chester Lee <chesterlee@chestersmacbook.lan>
2023-11-19 23:44:42 +0800

    test2

--- components/Homepage.js
+++ components/Homepage.js
@@ -3,7 +3,9 @@ import Navbar from "./Navbar";
 const Homepage = () => {
     return(
         <div className="flex flex-col h-screen bg-[#F9D548]">
+            
             <Navbar />
+            <p> hello</p>
             
         </div>
     );
