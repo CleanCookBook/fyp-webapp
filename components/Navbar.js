2f35661211f6f30be80adb28b69b6ebfe40ac161
jannath2001 <jannath2001@gmail.com>
2023-12-06 04:32:02 +0800

    Added filter portion
    I also tweaked some navbar portions along with the homepage

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,51 @@
+import Image from 'next/image';
+import { useState } from 'react';
+
 const Navbar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
-    <nav className=" flex bg-blue-950 h-[80px] items-center">
+    <nav className="flex bg-blue-950 h-[80px] items-center">
       <div className="text-white flex w-1/2 justify-start font-black text-2xl px-10">
-        <p>Logo Image Here</p>
+        {/* Circular Logo Image */}
+        <div className="rounded-full overflow-hidden border border-white w-16 h-16 flex-shrink-0">
+          <Image
+            src="/logo.jpg" // Update the path based on your actual file structure
+            alt="Logo"
+            width={80} // Adjust the width as needed
+            height={80} // Adjust the height as needed
+          />
+        </div>
       </div>
       <div className="container mx-auto">
         <ul>
           <li className="text-white font-bold flex justify-end">
-            <a className=" px-4">Recipes</a>
-            <a className=" px-4">About Us</a>
-            <a className=" px-4">Meal Plans</a>
-            <a className=" pr-10 pl-4">Account</a>
+            <a className="px-4">Recipes</a>
+            <a className="px-4">About Us</a>
+            <a className="px-4">Notification</a>
+            <a className="px-4">NewsFeed</a>
+            <a className="px-4">Meal Plans</a>
+            <div className="relative inline-block text-left">
+              <button onClick={toggleDropdown} className="px-4 focus:outline-none">
+                Account
+              </button>
+              {isDropdownOpen && (
+                <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                  <div className="py-1">
+                    <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                      Profile
+                    </a>
+                    <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                      About Me
+                    </a>
+                  </div>
+                </div>
+              )}
+            </div>
           </li>
         </ul>
       </div>
