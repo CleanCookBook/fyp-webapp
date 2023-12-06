f6c7b7b6df20a6578eb1e377da513c49fff28ac9
Chester Lee <chester@Chesters-MacBook-Pro.local>
2023-12-06 17:54:02 +0800

    added hover and made elements in navbar buttons.

--- components/Homepage.js
+++ components/Homepage.js
@@ -1,7 +1,7 @@
-import Image from 'next/image';
-import { useState } from 'react';
+import Image from "next/image";
+import { useState } from "react";
 import Footer from "./Footer";
-import Modal from './Modal'; // Adjust the path based on your file structure
+import Modal from "./Modal"; // Adjust the path based on your file structure
 import Navbar from "./Navbar";
 
 const Homepage = () => {
@@ -19,9 +19,7 @@ const Homepage = () => {
     <div className="flex flex-col h-screen bg-[#F9D548]">
       <Navbar />
       <div className="flex flex-col h-screen bg-[#F9D548] text-[#0A2A67] justify-center items-center">
-        <h1 className="text-7xl font-black py-5">
-          Whats Cooking Today?
-        </h1>
+        <h1 className="text-7xl font-black py-5">Whats Cooking Today?</h1>
         <div className="w-[748px] bg-white rounded-[20px] flex items-center text-sm p-2 pl-9 text-stone-300">
           <p>Search our library of recipes</p>
           <div className="ml-auto m-[5px]">
@@ -31,7 +29,7 @@ const Homepage = () => {
                 alt="Filter"
                 width={20}
                 height={20}
-                style={{ filter: 'brightness(0)' }}
+                style={{ filter: "brightness(0)" }}
               />
             </button>
           </div>
--- components/Modal.js
+++ components/Modal.js
@@ -1,9 +1,8 @@
 // Modal.js
 const Modal = ({ onClose }) => {
-
   const handleSubmit = () => {
     // Add your submit logic here
-    console.log('Submit clicked');
+    console.log("Submit clicked");
   };
 
   const handleClear = () => {
@@ -12,107 +11,128 @@ const Modal = ({ onClose }) => {
     checkboxes.forEach((checkbox) => {
       checkbox.checked = false;
     });
-  
+
     // Logic to reset dropdowns to default value
-    const dropdowns = document.querySelectorAll('select');
+    const dropdowns = document.querySelectorAll("select");
     dropdowns.forEach((dropdown) => {
       // Set the first option as the default value
       dropdown.value = dropdown.options[0].value;
     });
-  
+
     // Logic to clear calories input box
     const caloriesInput = document.querySelector('input[type="number"]');
-    caloriesInput.value = ''; // Clear the input box
-  
+    caloriesInput.value = ""; // Clear the input box
+
     // Add any additional logic as needed
-    console.log('Clear clicked');
+    console.log("Clear clicked");
   };
-  
-  
-  
 
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
       <div className="bg-white p-8 rounded-3xl shadow-2xl">
         {/* Modal content goes here */}
         <form onSubmit={handleSubmit}>
-        <h1 className="text-2xl font-bold mb-4 underline text-blue-900">Filter</h1>
-        <div className="flex">
-          {/* Left Section: Dietary Preference */}
-          <div className="flex flex-col pr-4 border-r border-blue-900">
-            <h2 className="text-lg font-semibold mb-2 text-blue-900">Dietary Preference</h2>
-            <label className="flex items-center mb-1">
-              <input type="checkbox" className="mr-2 bg-gray-300"/> Vegan
-            </label>
-            <label className="flex items-center">
-              <input type="checkbox" className="mr-2" /> Halal
-            </label>
-            <label className="flex items-center">
-              <input type="checkbox" className="mr-2" /> Pescatarian
-            </label>
-            <label className="flex items-center">
-              <input type="checkbox" className="mr-2" /> Gluten-Free
-            </label>
-            {/* Separator line */}
-            <div className="border-b my-2 border-blue-900"></div>
-            {/* Cooking Time heading */}
-            <h2 className="text-lg font-semibold mb-2 text-blue-900">Cooking Time</h2>
-            {/* Cooking Time dropdown */}
-            <select className="mb-2">
-              <option value="10">--- SELECT TIME ---</option>
-              <option value="10">10 mins</option>
-              <option value="20">20 mins</option>
-              <option value="30">30 mins</option>
-              <option value="60">1 hr</option>
-            </select>
-            {/* Separator line */}
-            <div className="border-b my-2 border-blue-900"></div>
-            {/* Calories heading */}
-            <h2 className="text-lg font-semibold mb-2 text-blue-900">Calories</h2>
-            {/* Calories input box */}
-            <div className="flex items-center mb-1">
-              <input type="number" className="mr-2 p-2 border rounded" placeholder="Enter calories" />
-              {/* Calories units dropdown */}
-              <select>
-                <option value="cal">cal</option>
-                <option value="kcal">kcal</option>
+          <h1 className="text-2xl font-bold mb-4 underline text-blue-900">
+            Filter
+          </h1>
+          <div className="flex">
+            {/* Left Section: Dietary Preference */}
+            <div className="flex flex-col pr-4 border-r border-blue-900">
+              <h2 className="text-lg font-semibold mb-2 text-blue-900">
+                Dietary Preference
+              </h2>
+              <label className="flex items-center mb-1">
+                <input type="checkbox" className="mr-2 bg-gray-300" /> Vegan
+              </label>
+              <label className="flex items-center">
+                <input type="checkbox" className="mr-2" /> Halal
+              </label>
+              <label className="flex items-center">
+                <input type="checkbox" className="mr-2" /> Pescatarian
+              </label>
+              <label className="flex items-center">
+                <input type="checkbox" className="mr-2" /> Gluten-Free
+              </label>
+              {/* Separator line */}
+              <div className="border-b my-2 border-blue-900"></div>
+              {/* Cooking Time heading */}
+              <h2 className="text-lg font-semibold mb-2 text-blue-900">
+                Cooking Time
+              </h2>
+              {/* Cooking Time dropdown */}
+              <select className="mb-2">
+                <option value="10">--- SELECT TIME ---</option>
+                <option value="10">10 mins</option>
+                <option value="20">20 mins</option>
+                <option value="30">30 mins</option>
+                <option value="60">1 hr</option>
               </select>
+              {/* Separator line */}
+              <div className="border-b my-2 border-blue-900"></div>
+              {/* Calories heading */}
+              <h2 className="text-lg font-semibold mb-2 text-blue-900">
+                Calories
+              </h2>
+              {/* Calories input box */}
+              <div className="flex items-center mb-1">
+                <input
+                  type="number"
+                  className="mr-2 p-2 border rounded"
+                  placeholder="Enter calories"
+                />
+                {/* Calories units dropdown */}
+                <select>
+                  <option value="cal">cal</option>
+                  <option value="kcal">kcal</option>
+                </select>
+              </div>
             </div>
-          
+
+            {/* Right Section: Allergy */}
+            <div className="flex flex-col pl-4">
+              <h2 className="text-lg font-semibold mb-2 text-blue-900">
+                Allergy
+              </h2>
+              <label className="flex items-center mb-1">
+                <input type="checkbox" className="mr-2" /> Nuts
+              </label>
+              <label className="flex items-center">
+                <input type="checkbox" className="mr-2" /> Dairy-free
+              </label>
+              <label className="flex items-center">
+                <input type="checkbox" className="mr-2" /> Seafood
+              </label>
+              <label className="flex items-center">
+                <input type="checkbox" className="mr-2" /> Eggs
+              </label>
+              <label className="flex items-center">
+                <input type="checkbox" className="mr-2" /> Soy
+              </label>
+              {/* Separator line */}
+              <div className="border-b my-2 border-blue-900"></div>
+            </div>
           </div>
 
-          {/* Right Section: Allergy */}
-          <div className="flex flex-col pl-4">
-            <h2 className="text-lg font-semibold mb-2 text-blue-900">Allergy</h2>
-            <label className="flex items-center mb-1">
-              <input type="checkbox" className="mr-2" /> Nuts
-            </label>
-            <label className="flex items-center">
-              <input type="checkbox" className="mr-2" /> Dairy-free
-            </label>
-            <label className="flex items-center">
-              <input type="checkbox" className="mr-2" /> Seafood
-            </label>
-            <label className="flex items-center">
-              <input type="checkbox" className="mr-2" /> Eggs
-            </label>
-            <label className="flex items-center">
-              <input type="checkbox" className="mr-2" /> Soy
-            </label>
-            {/* Separator line */}
-            <div className="border-b my-2 border-blue-900"></div>
-          </div>
-        </div>
-
-        {/* Button Section */}
-        <div className="flex justify-end mt-4 space-x-4">
-            <button type="button" onClick={handleClear} className="text-blue-900 hover:text-blue-700">
+          {/* Button Section */}
+          <div className="flex justify-end mt-4 space-x-4">
+            <button
+              type="button"
+              onClick={handleClear}
+              className="font-bold text-blue-900 hover:text-blue-700"
+            >
               Clear
             </button>
-            <button type="submit" className="bg-blue-900 text-white px-4 py-2 rounded">
+            <button
+              type="submit"
+              className="font-bold bg-blue-900 hover:bg-[#1c57b1] text-white px-4 py-2 rounded"
+            >
               Submit
             </button>
-            <button type="button" onClick={onClose} className="bg-blue-900 text-white px-4 py-2 rounded">
+            <button
+              type="button"
+              onClick={onClose}
+              className="font-bold bg-blue-900 hover:bg-[#1c57b1] text-white px-4 py-2 rounded"
+            >
               Close
             </button>
           </div>
