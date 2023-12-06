e3d96957cf2372340823c86dbf9f5b50c19d7f29
jannath2001 <jannath2001@gmail.com>
2023-12-06 16:20:22 +0800

    solving the useState issue
    after getting this file filter should work properly

--- app/page.js
+++ app/page.js
@@ -1,19 +1,36 @@
-import Welcome from "@/components/Welcome";
-import Signup from "@/components/Signup";
-import Login from "@/components/Login";
+// app/page.js
+"use client";
 import Homepage from "@/components/Homepage";
-import Image from "next/image";
-import Head from "next/head";
-import About from "@/components/About";
+import Signup from "@/components/Signup";
+//This too yall can delete
+//import { useEffect, useState } from 'react';
 
-export default function Home() {
+const Home = () => {
+
+  //Yall can delete i leave here cos this is some of my backened i will need for my future reference yeah
+/* const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from the API on the client side
+    fetch('http://localhost:3001/api/users')
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data.users);
+      })
+      .catch((error) => {
+        console.error('Error fetching users:', error);
+      });
+  }, []);  */
+
   return (
     <main className="">
-      {/* <Welcome /> */}
-      {/* <Login /> */}
-      {/* <Signup /> */}
-      {/* <Homepage /> */}
-      <About />
+
+      <Signup />
+      <Homepage />
+   
+    
     </main>
   );
-}
+};
+
+export default Home;
