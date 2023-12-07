5485cf4caad92f1501e950759a3d1aca5d3fba71
kathyna24 <116356291+kathyna24@users.noreply.github.com>
2023-12-07 22:54:46 +0800

    account page
    edited account page

--- components/Accountpage.js
+++ components/Accountpage.js
@@ -5,21 +5,21 @@ import Modal from "./Modal"; // Adjust the path based on your file structure
 import Navbar from "./Navbar";
 
 const Accountpage = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const openModal = () => {
+const openModal = () => {
     setIsModalOpen(true);
-  };
+};
 
-  const closeModal = () => {
+const closeModal = () => {
     setIsModalOpen(false);
-  };
+};
 
-  return (
-    <div className="flex flex-col h-screen bg-[#F9D548]">
-      <Navbar />
-      <div className=" lg:w-4/5 lg:pr-8 p-8 flex flex-col h-screen bg-[#F9D548] text-[#0A2A67] justify-start items-start">
-      <div className="w-full max-w-md flex flex-row">
+return (
+<div className="flex flex-col h-screen bg-[#F9D548]">
+    <Navbar />
+    <div className=" lg:w-4/5 lg:pr-8 p-8 flex flex-col h-screen bg-[#F9D548] text-[#0A2A67] justify-start items-start">
+    <div className="w-full max-w-md flex flex-row mt-7">
         <Image
             src="/logo.jpg"
             alt="Profile Picture"
@@ -27,18 +27,46 @@ const Accountpage = () => {
             height={150}
             className="rounded-full shadow-lg"
         />
-        <div className="flex flex-col mt-8 ml-8 gap-4">
-          <h1 className="flex flex-row text-xl font-bold">Clean Cook Book</h1>
-          <p className="text-sm text-gray-500">@Cleancookbook</p>
+        <div className="flex flex-col mt-9 ml-8 gap-4">
+          <h1 className="flex flex-row text-2xl font-bold text-black">Clean Cook Book</h1>
+          <p className="text-xl text-black">@Cleancookbook</p>
         </div>
     </div>
+
     <div>
-        <h2 className="text-xl underline font-bold left-10 mt-5">General Setting</h2> 
+        <h2 className="text-xl underline font-bold left-10 mt-9">General Settings</h2>
+        <div className="flex flex-row mt-9 gap-4">
+            <p className="flex flex-row text-xl text-black font-semibold">Name :</p>
+            <p className="text-xl text-black">Clean Cook Book</p>
         </div>
+        <div className="flex flex-row mt-9 gap-4">
+            <p className="flex flex-row text-xl text-black font-semibold">Date of Birth :</p>
+            <p className="text-xl text-black">12/7/2023</p>
+        </div>
+        <div className="flex flex-row mt-9 gap-4">
+            <p className="flex flex-row text-xl text-black font-semibold">Gender :</p>
+            <p className="text-xl text-black">Male</p>
+        </div>
+        <div className="flex flex-row mt-9 gap-4">
+            <p className="flex flex-row text-xl text-black font-semibold">E-mail :</p>
+            <p className="text-xl text-black">Cleancookbook@gmail.com</p>
+        </div>
+        <div className="flex flex-row mt-28 gap-4">
+            <button className="w-[250px] h-9 bg-blue-950 hover:bg-[#154083] text-white font-bold text-xl rounded-[10px] shadow">
+                <a href="#">Edit</a>
+            </button>
+
+            <button className="w-[250px] h-9 bg-blue-950 hover:bg-[#154083] text-white font-bold text-xl rounded-[10px] shadow">
+                <a href="#">Reset Password</a>
+            </button>
+        </div>
+        
+      
     </div>
-      <Footer />
-      {isModalOpen && <Modal onClose={closeModal} />}
     </div>
+    <Footer />
+        {isModalOpen && <Modal onClose={closeModal} />}
+</div>
   );
 };
 
