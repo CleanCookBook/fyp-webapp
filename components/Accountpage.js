bb1275dab9d2c95a760489751c6af113451e8499
John <147623698+HtetNaingMin@users.noreply.github.com>
2023-12-07 21:53:40 +0800

    logo added

--- components/Accountpage.js
+++ components/Accountpage.js
@@ -18,53 +18,22 @@ const Accountpage = () => {
   return (
     <div className="flex flex-col h-screen bg-[#F9D548]">
       <Navbar />
-      <div className="flex flex-col h-screen bg-[#F9D548] text-[#0A2A67] justify-center items-center">
+      <div className=" lg:w-4/5 lg:pr-8 p-8 flex flex-col h-screen bg-[#F9D548] text-[#0A2A67] justify-start items-start">
       <div className="w-full max-w-md">
-        <h2 className="text-xl underline font-bold left-10">Hello!</h2>
-        
-        <form action="/update-account" method="post">
-          <div className="flex flex-row mb-4">
-            <label className="w-1/3">Name:</label>
-            <input type="text" name="name" className="w-2/3 border p-2" />
-          </div>
+            <Image
+                src="/logo.jpg"
+                alt="Description"
+                width={150}
+                height={150}
+                className="rounded-full shadow-lg"
+            />
+            <div className="flex flex-col gap-4">
+            <h1 className="text-xl font-bold">Chester Lee</h1>
+            <p className="text-sm text-gray-500">@CLJW</p>
+            </div>
 
-          <div className="flex flex-row mb-4">
-            <label className="w-1/3">Date of Birth:</label>
-            <input type="date" name="dob" className="w-2/3 border p-2" />
-          </div>
-
-          <div className="flex flex-row mb-4">
-            <label className="w-1/3">Gender:</label>
-            <select name="gender" className="w-2/3 border p-2">
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-              <option value="other">Other</option>
-            </select>
-          </div>
-
-          <div className="flex flex-row mb-4">
-            <label className="w-1/3">E-mail:</label>
-            <input type="email" name="email" className="w-2/3 border p-2" />
-          </div>
-
-          <div className="flex flex-row mb-4">
-            <label className="w-1/3">Height:</label>
-            <input type="number" name="height" className="w-2/3 border p-2" />
-          </div>
-
-          <div className="flex flex-row mb-4">
-            <label className="w-1/3">Weight:</label>
-            <input type="number" name="weight" className="w-2/3 border p-2" />
-          </div>
-        
-          <div className="pt-3">
-            <button type="submit" className="w-[170px] h-7 bg-blue-950 hover:bg-[#154083] text-white font-bold text-sl rounded-[10px] shadow">
-            Confirm Update
-            </button>
-          </div>
-
-        </form>
-      </div>
+        <h2 className="text-xl underline font-bold left-10 mt-5">General Setting</h2> 
+        </div>
     </div>
       <Footer />
       {isModalOpen && <Modal onClose={closeModal} />}
