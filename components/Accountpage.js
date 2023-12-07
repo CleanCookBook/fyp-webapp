0e9f89e1d812f808fad980d35bf909f0d69e2ab2
kathyna24 <116356291+kathyna24@users.noreply.github.com>
2023-12-07 21:59:41 +0800

    updated account
    updated account

--- components/Accountpage.js
+++ components/Accountpage.js
@@ -19,19 +19,20 @@ const Accountpage = () => {
     <div className="flex flex-col h-screen bg-[#F9D548]">
       <Navbar />
       <div className=" lg:w-4/5 lg:pr-8 p-8 flex flex-col h-screen bg-[#F9D548] text-[#0A2A67] justify-start items-start">
-      <div className="w-full max-w-md">
-            <Image
-                src="/logo.jpg"
-                alt="Description"
-                width={150}
-                height={150}
-                className="rounded-full shadow-lg"
-            />
-            <div className="flex flex-col gap-4">
-            <h1 className="text-xl font-bold">Chester Lee</h1>
-            <p className="text-sm text-gray-500">@CLJW</p>
-            </div>
-
+      <div className="w-full max-w-md flex flex-row">
+        <Image
+            src="/logo.jpg"
+            alt="Profile Picture"
+            width={150}
+            height={150}
+            className="rounded-full shadow-lg"
+        />
+        <div className="flex flex-col mt-8 ml-8 gap-4">
+          <h1 className="flex flex-row text-xl font-bold">Clean Cook Book</h1>
+          <p className="text-sm text-gray-500">@Cleancookbook</p>
+        </div>
+    </div>
+    <div>
         <h2 className="text-xl underline font-bold left-10 mt-5">General Setting</h2> 
         </div>
     </div>
