9b2ef04e1c56eaa7089897161a618658fc80da0d
John <147623698+HtetNaingMin@users.noreply.github.com>
2023-12-15 15:10:53 +0800

    added page.js for lowcardmeal3 and 1

---
+++ app/LowCardMealPlan1/page.js
@@ -0,0 +1,11 @@
+"use client";
+import LowCarbsMealPlan1 from '@/components/LowCarbsMealPlan1';
+const mplowcarb1 = () => {
+return (
+    <main>
+    <LowCarbsMealPlan1/>
+    </main>
+);
+};
+
+export default mplowcarb1 ;
