5100415a8cc95b4035a8c52787b4fa607a49e009
jannath2001 <jannath2001@gmail.com>
2023-12-18 14:52:48 +0800

    EditAccount is done woohoo

--- components/Signup.js
+++ components/Signup.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 const Signup = () => {
   const router = useRouter();
