8529ec3f365b31f0139f76b3770f55c7c0f12d9f
jannath2001 <jannath2001@gmail.com>
2023-12-19 17:51:28 +0800

    Search and get the result

--- app/detailRecipe/page.js
+++ app/detailRecipe/page.js
@@ -1,11 +1,40 @@
 "use client";
+import ABCsoup from '@/components/ABCsoup';
+import Bibimbap from '@/components/Bibimbap';
 import RecipeDetails from '@/components/RecipePage';
+import SteamedEgg from '@/components/Steamedegg';
+import VegetarianBurrito from '@/components/VegetarianBurrito';
+import { useSearchParams } from 'next/navigation';
+
 const RecipeDet = () => {
+  const searchParams = useSearchParams();
+  const recipeName = searchParams.get('recipeName');
+
+    const renderRecipeComponent = () => {
+        switch (recipeName) {
+          case 'Homemade Fish & Chips':
+            return <RecipeDetails/>;
+          case 'Vegetarian Burrito':
+            return <VegetarianBurrito/>;
+          case 'Steamed Egg':
+            return  <SteamedEgg/>;
+          case 'Bibimbap':
+            return <Bibimbap/>;
+          case 'ABC Soup':
+            return <ABCsoup/>;
+          default:
+            return <p>Recipe not found</p>;
+        }
+      };
 return (
     <main>
-    <RecipeDetails/>
+     {renderRecipeComponent()}
+
+
+
+    
     </main>
 );
 };
 
-export default RecipeDet  ;
+export default RecipeDet ;
--- components/Homepage.js
+++ components/Homepage.js
@@ -1,11 +1,19 @@
 import Image from "next/image";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useState } from "react";
 import Footer from "./Footer";
 import Modal from "./Modal"; // Adjust the path based on your file structure
 import Navbar from "./Navbar";
 
+
 const Homepage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const [noResults, setNoResults] = useState(false);
+  const router = useRouter();
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -15,13 +23,40 @@ const Homepage = () => {
     setIsModalOpen(false);
   };
 
+  const handleSearch = async () => {
+    // Fetch data from the API endpoint
+    const encodedQuery = encodeURIComponent(searchQuery);
+    const response = await fetch(`http://localhost:3001/api/search?query=${encodedQuery}`);
+    const data = await response.json();
+  
+    console.log('API Response:', data); // Add this line
+  
+    // Update state with search results
+    setSearchResults(data);
+  
+    // Check for empty results (empty array or empty object)
+    if (!Array.isArray(data) || data.length === 0) {
+      setNoResults(true);
+    } else {
+      // Recipe found, navigate to RecipeList page
+      const queryString = new URLSearchParams({ searchInput: searchQuery,searchResults: JSON.stringify(data)});
+      const newPathname = '/recipelist?' + queryString;
+      router.push(newPathname);// Change '/recipelist' to your actual RecipeList page path
+    }
+  };
+
   return (
     <div className="flex flex-col h-screen bg-[#F9D548]">
       <Navbar />
       <div className="flex flex-col h-screen bg-[#F9D548] text-[#0A2A67] justify-center items-center">
-        <h1 className="text-7xl font-black py-5">Whats Cooking Today?</h1>
+        <h1 className="text-7xl font-black py-5">What's Cooking Today?</h1>
         <div className="w-[748px] bg-white rounded-[20px] flex items-center text-sm p-2 pl-9 text-stone-300">
-          <p>Search our library of recipes</p>
+          <input
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            placeholder="Search our library of recipes"
+          />
           <div className="ml-auto m-[5px]">
             <button onClick={openModal}>
               <Image
@@ -33,7 +68,11 @@ const Homepage = () => {
               />
             </button>
           </div>
+          <button className="ml-2" onClick={handleSearch}>
+            Search
+          </button>
         </div>
+        {noResults && <p className="text-red-500 mt-2">No such recipe</p>}
       </div>
       <Footer />
       {isModalOpen && <Modal onClose={closeModal} />}
