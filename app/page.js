a884bbf997218879c08b0a23bbaaaee7deb13d95
Chester Lee <chesterleejw@gmail.com>
2024-01-04 18:37:48 +0800

    Create business sign up page 2

--- app/page.js
+++ app/page.js
@@ -1,15 +1,13 @@
 // app/page.js
 "use client";
 import Welcome from "@/components/Welcome";
-
+import BusinessPartnerSignup from "@/components/BusinessPartnerSignup";
 
 const Home = () => {
-  
   return (
     <main className="">
-
-       <Welcome /> 
-
+      <BusinessPartnerSignup />
+      <Welcome />
     </main>
   );
 };
