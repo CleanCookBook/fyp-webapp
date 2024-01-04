3fb3700ea7dc494345481ee05aa0a017c8beb64c
Chester Lee <chesterleejw@gmail.com>
2024-01-05 01:45:51 +0800

    Create BP homepage

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 // app/page.js
 "use client";
 import Welcome from "@/components/Welcome";
+import BPHomepage from "@/components/BPHomepage";
 
 const Home = () => {
   return (
     <main className="">
-      <Welcome />
+      <BPHomepage />
+      {/* <Welcome /> */}
     </main>
   );
 };
