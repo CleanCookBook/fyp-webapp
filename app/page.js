94c25fc8d8acc937385c56c364ebba0f96c7b149
Chester Lee <chesterleejw@gmail.com>
2024-01-05 17:59:48 +0800

    Create view announcement page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 // app/page.js
 "use client";
 import Welcome from "@/components/Welcome";
+import BPAnnouncement from "@/components/BPAnnouncement";
 
 const Home = () => {
   return (
     <main className="">
-      <Welcome />
+      {/* <Welcome /> */}
+      <BPAnnouncement />
     </main>
   );
 };
