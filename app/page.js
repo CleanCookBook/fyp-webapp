bbd3fd5a7bc7d69ed783687cf4907656419af7f9
Chester Lee <chesterleejw@gmail.com>
2024-01-22 21:16:22 +0800

    Create user feedback page

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
 // app/page.js
 "use client";
+import CreateUserFeedback from "@/components/CreateUserFeedback";
 import Welcome from "@/components/Welcome";
 
 const Home = () => {
   return (
     <main className="">
       {/* <Welcome /> */}
-      <Welcome/>
+      <CreateUserFeedback />
     </main>
   );
 };
