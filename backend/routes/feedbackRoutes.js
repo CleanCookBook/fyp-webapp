4556e300ccbd9a116fd7c9f82749d700a065903a
kathyna24 <116356291+kathyna24@users.noreply.github.com>
2024-01-24 20:39:50 +0800

    feedback updated

--- backend/routes/feedbackRoutes.js
+++ backend/routes/feedbackRoutes.js
@@ -1,21 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
-
-const isAuthenticated = (req, res, next) => {
-  if (req.session && req.session.userId) {
-    req.userId = req.session.userId;
-    return next();
-  } else {
-    res.status(401).json({ error: 'Not authenticated' });
-  }
-};
+const isAuthenticated = require("../authMiddleware");
 
 // API endpoint for handling feedback submissions
 router.post('/feedbackForm', isAuthenticated, (req, res) => {
   console.log("testing");
-  const { userId, feedbackType, comments } = req.body;
 
+  const userId = req.userId;
+  const { feedbackType, comments } = req.body;
+  
   // Insert feedback data into the Feedback table
   db.run('INSERT INTO Feedback (UserId, FeedbackType, Feedback) VALUES (?, ?, ?)', [userId, feedbackType, comments], function (err) {
     if (err) {
@@ -27,5 +21,25 @@ router.post('/feedbackForm', isAuthenticated, (req, res) => {
     }
   });
 });
+
+router.get("/user-feedback", async (req, res) => {
+  try {
+    const users = await new Promise((resolve, reject) => {
+      db.all("SELECT * FROM Feedback ", (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+
+    res.send(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 
 module.exports = router;
