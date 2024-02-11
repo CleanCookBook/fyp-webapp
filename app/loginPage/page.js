ca3e7ed29e45ea22ff3b1542aa5be20cc2f1f36b
jannath2001 <116989317+jannath2001@users.noreply.github.com>
2024-02-11 23:56:54 +0800

    Update page.js

--- app/loginPage/page.js
+++ app/loginPage/page.js
@@ -18,7 +18,7 @@ const Login = () => {
     try {
       setLoading(true);
 
-      const response = await fetch("http://localhost:3001/api/login", {
+      const response = await fetch("https://ccb-backendd.onrender.com/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
